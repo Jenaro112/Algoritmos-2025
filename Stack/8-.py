2c31f9c5f24e27002ba897b0f469beb52254e181
Jenaro112 <jenarogg2020@gmail.com>
2025-04-28 17:46:37 -0300

    new files

---
+++ Stack/8-.py
@@ -0,0 +1,8 @@
+""" 8. Se dispone de las lista de superhéroes y villanos de la saga de Marvel Cinematic Universe (MCU) de los que contamos con la información de nombre del personaje y año de la primera película en la que apareció; a partir de estos resolver las siguientes actividades:
+    a. realizar un listado ascendente de los personajes ordenados por su nombre;
+    b. indicar quien fue el primer y el último personaje en aparecer en una película sin realizar un recorrido de la lista (podrían ser más de uno tanto el primer como el último);
+    c. realizar un listado de los personajes ordenados de manera descendente por año de aparición;
+    . calcular el rango de años entre el primer personaje en aparecer y el último;
+    e. determinar si los personajes Capitan America y Rocket Raccoon están en la lista y en qué año aparecieron. """
+    
+superheroes = []
