47d06f47d9151942bf3233f60d80c869056b7822
Jenaro <JenaroGG2020@gmail.com>
2025-06-05 12:54:11 -0300

    upload

--- Queue/8-.py
+++ Queue/8-.py
@@ -1,3 +1,30 @@
 """
 Realizar un algoritmo que mantenga ordenado los elementos agregados a una cola, utilizando solo una cola como estructura auxiliar.
 """
+
+from queue_ import Queue
+
+def burbuja(queue: Queue) -> Queue:
+    n = queue.size()
+    for j in range(n):
+        previo = queue.attention()
+        for j in range(queue.size()):
+            actual = queue.attention()
+            if previo > actual:
+                queue.arrive(actual)
+            else:
+                queue.arrive(previo)
+                previo = actual
+        queue.arrive(previo)
+    return queue
+
+cola = Queue()
+for i in [5, 3, 8, 6, 2]:
+    cola.arrive(i)
+
+print("Cola sin ordenar:")
+cola.show()
+
+cola_ordenada = burbuja(cola)
+print("Cola ordenada:")
+cola_ordenada.show()
--- Queue/9-.py
+++ Queue/9-.py
@@ -2,18 +2,42 @@
 Dada una cola de valores enteros calcular su rango y contar cuántos elementos negativos hay.
 """
 
-from queue import Queue
-from colorama import Fore, Style
+from queue_ import Queue
 from random import randint
 
-Numeros = Queue()
-for i in range(10):
-    Numeros.put(randint(-10, 10))
+def rango(cola: Queue) -> int:
 
-def rango (cola:Queue):
-    max = cola.queue[0]
-    min = cola.queue[0]
-    for i in range(cola.qsize()):
-        num = cola.get()
-        if num 
+    valor_min = float('inf')
+    valor_max = float('-inf')
+    
+    for _ in range(cola.size()):
+        valor = cola.attention()
+        valor_min = min(valor_min, valor)
+        valor_max = max(valor_max, valor)
+        cola.arrive(valor)  # Volvemos a agregar el elemento a la cola
+    
+    return valor_max - valor_min
 
+def negativos(cola: Queue) -> int:
+    contador = 0
+    
+    for j in range(cola.size()):
+        valor = cola.attention()
+        if valor < 0:
+            contador += 1
+        cola.arrive(valor)  # Volvemos a agregar el elemento a la cola
+    return contador
+
+cola = Queue()
+for _ in range(10):
+    cola.arrive(randint(-10, 10))
+
+print("-" * 50)
+print("Cola de enteros:")
+cola.show()
+
+print("-" * 50)
+print(f"Rango de la cola: {rango(cola)}")
+print("-" * 50)
+print(f"Número de elementos negativos en la cola: {negativos(cola)}")
+print("-" * 50)
